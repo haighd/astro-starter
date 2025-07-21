d478393bddc739676b1526d4763a94c5af735ec7
Dan <djhaight@gmail.com>
2025-07-21 13:41:31 -0400

    tutorial finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://shiny-starship-1c625d.netlify.app/",
+  integrations: [preact()]
+});
